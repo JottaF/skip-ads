b9f6f475db8a1abc366ba49f762b001002a862b9
Joao Felipe Lima De Oliveira <hepta.joao.o@AGU.GOV.BR>
2023-12-18 17:40:53 -0300

    feat: update README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # skip-ads
 A simple extension to skip Youtube's ads.
+
+## To use
+Open the [browser extension page](chrome://extensions/) and enable developer mode. Click on upload without compression and select the folder where you extracted the files.
--- content_script.js
+++ content_script.js
@@ -1,8 +1,6 @@
 "use strict"
 
 function findButtonAndClick(maxR = 50) {
-    console.log("procurando btn", maxR);
-
     let button = document.querySelector(".ytp-ad-skip-button-modern.ytp-button")
 
     if (!button && maxR > 0) {
@@ -19,7 +17,6 @@ function verifyChange() {
 }
 
 function findAdDiv(maxR = 50) {
-    console.log("procurando adiv", maxR);
     const div = document.querySelector(".video-ads.ytp-ad-module")
 
     if (!div && maxR > 0) {
@@ -32,29 +29,21 @@ function findAdDiv(maxR = 50) {
 }
 
 function init() {
-    console.log("inicio");
     const AdDiv = findAdDiv()
 
     const observer = new MutationObserver(() => {
         if (verifyChange()) {
-            console.log('Observou e vai clicar')
             const button = findButtonAndClick(20)
 
             try {
                 button.click()
-            } catch (e) {
-                console.log("Erro ao clicar no botão de anúncio")
-            }
+            } catch {}
         }
     })
 
-    // Define as opções do MutationObserver
     const config = { attributes: true, childList: true, subtree: true }
 
-    // Inicia a observação do elemento HTML com as opções definidas
     observer.observe(AdDiv, config)
-    console.log("fim");
-
 }
 
 if (document.readyState == "loading") document.addEventListener('DOMContentLoaded', init)
