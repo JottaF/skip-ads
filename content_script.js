a2329e50c51fc1751fffb6aa602de7bff36474d1
João Felipe Lima <joaofelipe.work@gmail.com>
2024-07-10 18:02:06 -0300

    feat: Fixed bug and added new feature

--- content_script.js
+++ content_script.js
@@ -1,52 +1,75 @@
-"use strict"
+"use strict";
 
-function findButtonAndClick(maxR = 50) {
-    let button = document.querySelector(".ytp-ad-skip-button-modern.ytp-button")
+function findButtonToSkip(maxR = 30) {
+  let button = document.querySelector(".ytp-skip-ad-button");
 
-    if (!button && maxR > 0) {
-        setTimeout(() => {
-           return findButtonAndClick(maxR - 1)
-        }, 150);
-    }
+  if (!button && maxR > 0) {
+    setTimeout(() => {
+      return findButtonToSkip(maxR - 1);
+    }, 150);
+  }
 
-    return button
+  return button;
+}
+
+function isAdWithoutButton() {
+  try {
+    const adText = document.querySelector(".ytp-preview-ad__text");
+
+    return adText.textContent == "O vídeo já vai começar";
+  } catch {
+    return null;
+  }
+}
+
+function accelerateAds() {
+  try {
+    const ad = document.querySelector(".video-stream.html5-main-video");
+    ad.playbackRate += 15;
+    ad.muted = true;
+  } catch {}
 }
 
 function verifyChange() {
-    return document.URL.includes("watch?")
+  return document.URL.includes("watch?");
 }
 
 function findAdDiv(maxR = 50) {
-    const div = document.querySelector(".video-ads.ytp-ad-module")
+  const div = document.querySelector(".video-ads.ytp-ad-module");
 
-    if (!div && maxR > 0) {
-        setTimeout(() => {
-            return findAdDiv(maxR - 1)
-        }, 150);
-    }
+  if (!div && maxR > 0) {
+    setTimeout(() => {
+      return findAdDiv(maxR - 1);
+    }, 150);
+  }
 
-    return div
+  return div;
 }
 
 function init() {
-    const AdDiv = findAdDiv()
+  const AdDiv = findAdDiv();
 
-    const observer = new MutationObserver(() => {
-        if (verifyChange()) {
-            const button = findButtonAndClick(20)
+  const observer = new MutationObserver(() => {
+    if (verifyChange()) {
+      if (isAdWithoutButton()) {
+        accelerateAds();
+      } else {
+        const button = findButtonToSkip(20);
 
-            try {
-                button.click()
-            } catch {}
-        }
-    })
+        try {
+          button.click();
+        } catch {}
+      }
+    }
+  });
 
-    const config = { attributes: true, childList: true, subtree: true }
+  const config = { attributes: true, childList: true, subtree: true };
 
-    try {
-        observer.observe(AdDiv, config)
-    } catch {}
+  try {
+    observer.observe(AdDiv, config);
+  } catch {}
 }
 
-if (document.readyState == "loading") document.addEventListener('DOMContentLoaded', init)
-else init()
+if (document.readyState == "loading")
+  document.addEventListener("DOMContentLoaded", init);
+else init();
